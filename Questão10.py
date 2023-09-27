0614c401dec249c8d742e78e1438a5c5dda7b6bd
mirelavicente <146104138+mirelavicente@users.noreply.github.com>
2023-09-27 12:30:15 -0300

    Update Questão10.py

--- Questão10.py
+++ Questão10.py
@@ -1,11 +1,8 @@
-# Solicite o peso em quilogramas e a altura em metros
 peso = float(input("Digite o peso em quilogramas: "))
 altura = float(input("Digite a altura em metros: "))
 
-# Calcule o IMC
 imc = peso / (altura ** 2)
 
-# Determine a condição de peso com base no IMC
 if imc < 18.5:
     condicao = "Abaixo do peso"
 elif 18.5 <= imc < 25:
@@ -15,5 +12,4 @@ elif 25 <= imc < 30:
 else:
     condicao = "Obeso"
 
-# Imprima a condição de peso
 print(f"O IMC é {imc:.2f} e a condição é: {condicao}")
