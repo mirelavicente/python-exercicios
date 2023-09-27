f7f8e6e03b070d217ec944acc450754879e95728
mirelavicente <146104138+mirelavicente@users.noreply.github.com>
2023-09-27 12:29:38 -0300

    Update Questão9.py

--- Questão9.py
+++ Questão9.py
@@ -1,13 +1,11 @@
-# Solicite a altura e o sexo da pessoa
 altura = float(input("Digite a altura em metros: "))
 sexo = input("Digite o sexo (M para masculino, F para feminino): ")
 
-# Verifique o sexo e calcule o peso ideal de acordo com a fórmula correspondente
 if sexo.upper() == "M":
-    peso_ideal = (72.7 * altura) - 58
-    print(f"O peso ideal para um homem de {altura} metros é {peso_ideal:.2f} kg.")
+    pesoideal = (72.7 * altura) - 58
+    print(f"O peso ideal para um homem de {altura} metros é {pesoideal:.2f} kg.")
 elif sexo.upper() == "F":
-    peso_ideal = (62.1 * altura) - 44.7
-    print(f"O peso ideal para uma mulher de {altura} metros é {peso_ideal:.2f} kg.")
+    pesoideal = (62.1 * altura) - 44.7
+    print(f"O peso ideal para uma mulher de {altura} metros é {pesoideal:.2f} kg.")
 else:
     print("Sexo não reconhecido. Por favor, digite 'M' para masculino ou 'F' para feminino.")
