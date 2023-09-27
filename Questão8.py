015b06934942268c9646eccf14b7e99d82389b3a
mirelavicente <146104138+mirelavicente@users.noreply.github.com>
2023-09-27 12:28:37 -0300

    Update Questão8.py

--- Questão8.py
+++ Questão8.py
@@ -3,10 +3,10 @@ b = int(input("Digite o segundo valor inteiro: "))
 c = int(input("Digite o terceiro valor inteiro: "))
 
 if a != b != c != a:
-    max_value = max(a, b, c)
-    min_value = min(a, b, c)
-    middle_value = (a + b + c) - max_value - min_value
+    maxvalue = max(a, b, c)
+    minvalue = min(a, b, c)
+    middlevalue = (a + b + c) - maxvalue - minvalue
     
-    print("Valores em ordem decrescente:", max_value, middle_value, min_value)
+    print("Valores em ordem decrescente:", maxvalue, middlevalue, minvalue)
 else:
     print("Os valores inseridos não são diferentes.")
